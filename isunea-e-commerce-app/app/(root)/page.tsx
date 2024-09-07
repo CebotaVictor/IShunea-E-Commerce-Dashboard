3230d2be6e433b60edd85f7ad5005e49667e30bb
mistic1265 <cebotavictor14@gmail.com>
2024-09-07 15:41:07 +0300

    worked on dialog

--- isunea-e-commerce-app/app/(root)/page.tsx
+++ isunea-e-commerce-app/app/(root)/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
 
-export default function Home() {
+"use client"
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+
+const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if(!isOpen){
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
+
   return (
-    <div>
-        hello world
+    <div className="p-4">
+        Root page
     </div>
   );
 }
+
+
+
+export default SetupPage;
